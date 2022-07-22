6ffda88a0d482ec44f7f4e6f76320b50d7e1e59f
Sheffield Lok <61821574+sheffieldlok@users.noreply.github.com>
2022-07-22 16:43:41 +0800

    Time: 71 ms (59.30%), Space: 14.5 MB (66.69%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -1,6 +1,8 @@
+from collections import Counter
+
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        if len(s) == 0 or len(t) == 0 or len(t) != len(s): return False
+        #if len(s) == 0 or len(t) == 0 or len(t) != len(s): return False
         
         # 1. Put s into a dictionary, key = letter, value = # of times letter appears
         # 2. Compare t with s letter by letter:
@@ -8,25 +10,26 @@ class Solution:
         #   2.2 if letter in t does not exist: return false
         # 3. if dictionary is empty, return true. else return false.
         
-        dict = {}
+        #dict = {}
         
-        for i in range(len(s)):
-            if s[i] in dict: # if letter exists in dictionary already
-                dict[s[i]] += 1
-            else:   # new letter that is not in dict
-                dict[s[i]] = 1
+        #for i in range(len(s)):
+         #   if s[i] in dict: # if letter exists in dictionary already
+          #      dict[s[i]] += 1
+         #   else:   # new letter that is not in dict
+         #       dict[s[i]] = 1
             
-        for i in range(len(t)):
-            if t[i] in dict:
-                dict[t[i]] -= 1
-                if dict[t[i]] == 0:
-                    del dict[t[i]]
-            else: 
-                return False
+        #for i in range(len(t)):
+        #    if t[i] in dict:
+         #       dict[t[i]] -= 1
+         #       if dict[t[i]] == 0:
+         #           del dict[t[i]]
+        #    else: 
+          #      return False
             
-        if dict == {}: return True
-        else: return False
+        #if dict == {}: return True
+        #else: return False
         
+        return Counter(s) == Counter(t)
                     
         
         
