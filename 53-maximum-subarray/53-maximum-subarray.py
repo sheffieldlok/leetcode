3fbabb663146b13054038195c4371940646d60dc
Sheffield Lok <61821574+sheffieldlok@users.noreply.github.com>
2022-07-23 23:52:27 +0800

    Time: 799 ms (87.25%), Space: 28 MB (39.28%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -0,0 +1,10 @@
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
+        #if len(nums) == 0: return 0
+        #if len(nums) == 1: return nums[0]
+        
+        for i in range(1, len(nums)):
+            if nums[i-1] > 0:
+                nums[i] += nums[i-1]
+        return max(nums)
+        
