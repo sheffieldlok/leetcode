673812181e68f41eeaf0cd9b1cb1219284178e8c
Sheffield Lok <61821574+sheffieldlok@users.noreply.github.com>
2022-07-26 18:24:13 +0800

    Time: 165 ms (46.99%), Space: 18.4 MB (45.12%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -4,32 +4,49 @@ class Solution:
         # remember their initial positions (needed?)
         # group them together in new array of arrays
         
-        dict = {}
-        sorted_strs = [""] * len(strs)
+#         dict = {}
+#         sorted_strs = [""] * len(strs)
         
-        for i in range(len(strs)):
-            sorted_strs[i] = ''.join(sorted(strs[i])) # sort characters in anagram alphabetically: str > list > str
-            #print(sorted_strs[i])
-            if sorted_strs[i] not in dict:
-                dict[sorted_strs[i]] = [i]
-            elif sorted_strs[i] in dict:
-                dict[sorted_strs[i]].append(i)
+#         for i in range(len(strs)):
+#             sorted_strs[i] = ''.join(sorted(strs[i])) # sort characters in anagram alphabetically: str > list > str
+#             #print(sorted_strs[i])
+#             if sorted_strs[i] not in dict:
+#                 dict[sorted_strs[i]] = [i]
+#             elif sorted_strs[i] in dict:
+#                 dict[sorted_strs[i]].append(i)
                 
-            #print("dict: ", dict)
+#             #print("dict: ", dict)
             
-        result = []
+#         result = []
             
-        for value in dict.values():
-            grouped = []
-            #print(grouped)
-            for element in value:
-                #print(strs[element])
-                grouped.append(strs[element])
-            #print(grouped)
-            result.append(grouped)
+#         for value in dict.values():
+#             grouped = []
+#             #print(grouped)
+#             for element in value:
+#                 #print(strs[element])
+#                 grouped.append(strs[element])
+#             #print(grouped)
+#             result.append(grouped)
             
                        
-        return result
+#         return result
+        d = {}
+        for w in strs:
+            # prints each word in strs
+            #print("w: ", w)
+            
+            # get each character in w and puts as key
+            key = tuple(sorted(w))
+            #print("key: ", key)
+            
+            # puts those words with same letters as key in this dict
+            d[key] = d.get(key, []) + [w]
+            #print("d[key]: ", d[key])
+            #print("d.get(key, []): ", d.get(key, []))
+            #print("[w]: ", [w])
+            
+            print("")
+        return d.values()
             
         
         
