a0de5a0364976fb062fba3c1c7cb7ff39f81ea29
Sheffield Lok <61821574+sheffieldlok@users.noreply.github.com>
2022-07-27 12:29:31 +0800

    Time: 75 ms (69.65%), Space: 14.1 MB (94.04%) - LeetHub

--- 383-ransom-note/383-ransom-note.py
+++ 383-ransom-note/383-ransom-note.py
@@ -1,24 +1,26 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        dict_m = {}
+#         dict_m = {}
         
-        if len(ransomNote) > len(magazine): return False
+#         if len(ransomNote) > len(magazine): return False
         
-        for i in range(len(magazine)):
-            if magazine[i] not in dict_m:
-                dict_m[magazine[i]] = 1
-            else:
-                dict_m[magazine[i]] += 1
+#         for i in range(len(magazine)):
+#             if magazine[i] not in dict_m:
+#                 dict_m[magazine[i]] = 1
+#             else:
+#                 dict_m[magazine[i]] += 1
         
-        for j in range(len(ransomNote)):
-            if ransomNote[j] in dict_m:
-                dict_m[ransomNote[j]] -= 1
-                if dict_m[ransomNote[j]] < 0:
-                    return False
-            else:
-                return False
+#         for j in range(len(ransomNote)):
+#             if ransomNote[j] in dict_m:
+#                 dict_m[ransomNote[j]] -= 1
+#                 if dict_m[ransomNote[j]] < 0:
+#                     return False
+#             else:
+#                 return False
         
-        return True
+#         return True
+
+        return not collections.Counter(ransomNote) - collections.Counter(magazine)
         
         
         
