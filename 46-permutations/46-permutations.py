c25d5d1d6cda1842d5e9c29756e4d0d0dce3ba0b
Sheffield Lok <61821574+sheffieldlok@users.noreply.github.com>
2022-07-29 15:11:56 +0800

    Time: 87 ms (8.35%), Space: 13.9 MB (83.89%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,8 @@
+from itertools import permutations
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        perm = permutations(nums)
+        return list(list(perm))
+        
+        
