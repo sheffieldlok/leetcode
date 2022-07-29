f28f7008b4a0428418fa183bb475946324fb19a4
Sheffield Lok <61821574+sheffieldlok@users.noreply.github.com>
2022-07-29 12:43:48 +0800

    Time: 769 ms (22.62%), Space: 33.7 MB (5.07%) - LeetHub

---
+++ 77-combinations/77-combinations.py
@@ -0,0 +1,25 @@
+from itertools import permutations
+import numpy as np
+
+class Solution:
+    def combine(self, n: int, k: int) -> List[List[int]]:
+        res = []
+        
+        def backtrack(start, combi):
+            # base case
+            if len(combi) == k:
+                res.append(combi.copy())
+                return
+            
+            for i in range(start, n+1):
+                combi.append(i)
+                backtrack(i+1, combi)
+                combi.pop()
+                
+        backtrack(1, [])
+        return res
+                
+            
+            
+        
+        
