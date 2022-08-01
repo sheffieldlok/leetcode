c0c9ff0eade2df5398363117e040faa4ee050a35
Sheffield Lok <61821574+sheffieldlok@users.noreply.github.com>
2022-08-01 23:14:18 +0800

    Time: 164 ms (40.83%), Space: 14 MB (35.50%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,54 +1,54 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-#         # check each row
-#         for i in range(len(board)):
-#             dict = {}
-#             for j in range(len(board)):
-#                 if board[i][j] in dict and board[i][j] != ".":
-#                     return False
-#                 else:
-#                     dict[board[i][j]] = 1
-        
-#         # check each column
-#         for i in range(len(board)):
-#             dict = {}
-#             for j in range(len(board)):
-#                 if board[j][i] in dict and board[j][i] != ".":
-#                     return False
-#                 else:
-#                     dict[board[j][i]] = 1
-                    
-#         # check each 3x3 box
-#         for x in range(len(board) // 3):
-#             for i in range(len(board) // 3):
-#                 dict = {}
-#                 for j in range(len(board) // 3):
-#                     for k in range(len(board) // 3):
-#                         if board[j + 3*x][k + 3*i] in dict and board[j + 3*x][k+3*i] != ".":
-#                             return False
-#                         else:
-#                             dict[board[j+ 3*x][k+3*i]] = 1
-                       
-#         return True
-
-        # second soln (neetcode)
-        cols = collections.defaultdict(set)
-        rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set) # key = (r // 3, c // 3)
-        
-        for r in range(9):
-            for c in range(9):
-                if board[r][c] == ".": 
-                    continue
-                if (board[r][c] in rows[r] or
-                    board[r][c] in cols[c] or
-                    board[r][c] in squares[(r//3, c//3)]):
+        # check each row
+        for i in range(len(board)):
+            dict = {}
+            for j in range(len(board)):
+                if board[i][j] in dict and board[i][j] != ".":
                     return False
-                cols[c].add(board[r][c])
-                rows[r].add(board[r][c])
-                squares[r//3, c//3].add(board[r][c])
-                
+                else:
+                    dict[board[i][j]] = 1
+        
+        # check each column
+        for i in range(len(board)):
+            dict = {}
+            for j in range(len(board)):
+                if board[j][i] in dict and board[j][i] != ".":
+                    return False
+                else:
+                    dict[board[j][i]] = 1
+                    
+        # check each 3x3 box
+        for x in range(len(board) // 3):
+            for i in range(len(board) // 3):
+                dict = {}
+                for j in range(len(board) // 3):
+                    for k in range(len(board) // 3):
+                        if board[j + 3*x][k + 3*i] in dict and board[j + 3*x][k+3*i] != ".":
+                            return False
+                        else:
+                            dict[board[j+ 3*x][k+3*i]] = 1
+                       
         return True
+
+#         # second soln (neetcode)
+#         cols = collections.defaultdict(set)
+#         rows = collections.defaultdict(set)
+#         squares = collections.defaultdict(set) # key = (r // 3, c // 3)
+        
+#         for r in range(9):
+#             for c in range(9):
+#                 if board[r][c] == ".": 
+#                     continue
+#                 if (board[r][c] in rows[r] or
+#                     board[r][c] in cols[c] or
+#                     board[r][c] in squares[(r//3, c//3)]):
+#                     return False
+#                 cols[c].add(board[r][c])
+#                 rows[r].add(board[r][c])
+#                 squares[r//3, c//3].add(board[r][c])
+                
+#         return True
         
         
         
